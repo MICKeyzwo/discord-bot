fe3cd33e4c733cd95d36f69aa8a531e2e236fcf5
MICKeyzwo <singing.programmer@gmail.com>
2021-05-05 07:03:02 +0900

    evalに簡易ログ出力機能を追加

--- tests/commands/test-eval.js
+++ tests/commands/test-eval.js
@@ -6,4 +6,8 @@ const instance = new Eval();
 
 assert.equal(instance.exec(...`1 + 2`.split(/ +/)), '3');
 
+assert.equal(instance.exec(...`console.log(1)`.split(/ +/)), '1\nundefined');
+
+assert.equal(instance.exec(...`print(2)`.split(/ +/)), '2\nundefined');
+
 assert.notEqual(instance.exec(...`(() => {while(1) {} return 'ok'})()`.split(/ +/)), 'ok');
